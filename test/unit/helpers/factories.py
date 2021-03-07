3687c0a1ee8cb8f8ea598cf1a5e28658067a5abb
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-03-07 16:30:25 -0500

    Add Factory functions for testing with Transaction Model objects.

---
+++ test/unit/helpers/factories.py
@@ -0,0 +1,41 @@
+"""Factory methods for generating Test objects."""
+
+from datetime import date
+from uuid import uuid4
+
+from src import models
+
+
+class Location:
+
+    @staticmethod
+    def createOne() -> models.transaction.Location:
+        return models.transaction.Location(
+            address='123 Some Street',
+            city='Some City',
+            region='Some State',
+            postal_code='Some Zip Code',
+            country='Some Country',
+            latitude=1,
+            longitude=1,
+            store_number=None,
+        )
+
+
+class TransactionData:
+
+    @staticmethod
+    def createOne() -> models.TransactionData:
+        return models.TransactionData(
+            _id=uuid4(),
+            amount=1.00,
+            pending=False,
+            payee="A payee",
+            date_authorized=date.today(),
+            date_posted=date.today(),
+            spent_from_id=None,
+            account_id=uuid4(),
+            category=[],
+            location=Location.createOne(),
+            plaid_transaction_id="id_from_plaid"
+        )
