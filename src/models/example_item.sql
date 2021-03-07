c906dcff2e465beb5d81ceeac9e8fb08b2ff4d94
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-03-07 16:29:26 -0500

    Remove example files not needed.

--- src/models/example_item.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "example_item" (
-    "_id" uuid PRIMARY KEY,
-    "string" varchar(255),
-    "integer" smallint,
-    "json" jsonb
-);
--- src/models/simple.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "simple" (
-    "_id" uuid PRIMARY KEY,
-    "string" varchar(255),
-    "integer" smallint,
-    "array" varchar(255) []
-);
